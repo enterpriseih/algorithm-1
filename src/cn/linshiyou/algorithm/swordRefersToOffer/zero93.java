e845b8fbb3eede0c7abb255c1cb746ed86995091
linshiyou <2335404839@qq.com>
2022-09-23 16:36:25 +0800

    剑指 Offer II 093. 最长斐波那契数列

---
+++ src/cn/linshiyou/algorithm/swordRefersToOffer/zero93.java
@@ -0,0 +1,48 @@
+package cn.linshiyou.algorithm.swordRefersToOffer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: LJ
+ * @Description: 剑指 Offer II 093. 最长斐波那契数列
+ */
+public class zero93 {
+
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,4,5,6,7,8};
+//        int[] arr = {1,2,5};
+        Solution solution = new Solution();
+        System.out.println(solution.lenLongestFibSubseq(arr));
+
+    }
+
+    static class Solution {
+        public int lenLongestFibSubseq(int[] arr) {
+            Map<Integer, Integer> indices = new HashMap<>();
+            int n = arr.length;
+            for (int i=0; i<n; i++){
+                indices.put(arr[i], i);
+            }
+            int[][] dp = new int[n][n];
+            int ans = 0;
+            // arr[k] < arr[j] < arr[i]
+            for (int i=0; i<n; i++){
+                for (int j=i-1; j>=0 && arr[j]*2 > arr[i]; j--){
+                    int k = indices.getOrDefault(arr[i]-arr[j], -1);
+                    // 如果k!=-1则 表示找到了一个满足要arr[k] < arr[j] < arr[i]的K，所以长度至少是3
+                    if (k>=0){
+                        // 以arr[j]，arr[i]，作为数字的末尾 = 以 arr[k]，arr[j]最为数字的末尾+1
+                        dp[j][i] = Math.max(dp[k][j]+1, 3);
+                    }
+                    ans = Math.max(ans, dp[j][i]);
+                }
+            }
+            return ans;
+
+        }
+    }
+
+}
