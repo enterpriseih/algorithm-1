a51df76283ed2252c1934e973488f2ac908dbe78
linshiyou <2335404839@qq.com>
2022-10-13 23:28:53 +0800

    剑指 Offer II 099. 最小路径之和

---
+++ src/cn/linshiyou/algorithm/swordRefersToOffer/zero99.java
@@ -0,0 +1,40 @@
+package cn.linshiyou.algorithm.swordRefersToOffer;
+
+/**
+ * @Author: LJ
+ * @Description: 剑指 Offer II 099. 最小路径之和
+ *
+ *
+ * 动态规划：与98题有相似之处
+ */
+public class zero99 {
+
+    public static void main(String[] args) {
+
+        int[][] grid = {};
+
+        Solution solution = new Solution();
+        System.out.println(solution.minPathSum(grid));
+
+    }
+
+    static class Solution {
+
+        public int minPathSum(int[][] grid) {
+
+            int n = grid.length;
+            int m = grid[0].length;
+
+            for (int i=0; i<n; i++){
+                for (int j=0; j<m; j++){
+                    if (i==0 && j==0) continue;
+
+                    if (i==0) grid[i][j] = grid[i][j-1] + grid[i][j];
+                    else if (j==0) grid[i][j] = grid[i-1][j] + grid[i][j];
+                    else grid[i][j] = Math.min(grid[i][j-1], grid[i-1][j]) + grid[i][j]
+                }
+            }
+            return grid[n-1][m-1];
+        }
+    }
+}
