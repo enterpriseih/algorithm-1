61c04e3bb2f25a2df33517751f9723f4337cca2f
linshiyou <2335404839@qq.com>
2022-10-13 23:32:32 +0800

    剑指 Offer II 099. 最小路径之和

--- src/cn/linshiyou/algorithm/swordRefersToOffer/zero99.java
+++ src/cn/linshiyou/algorithm/swordRefersToOffer/zero99.java
@@ -31,7 +31,7 @@ public class zero99 {
 
                     if (i==0) grid[i][j] = grid[i][j-1] + grid[i][j];
                     else if (j==0) grid[i][j] = grid[i-1][j] + grid[i][j];
-                    else grid[i][j] = Math.min(grid[i][j-1], grid[i-1][j]) + grid[i][j]
+                    else grid[i][j] = Math.min(grid[i][j-1], grid[i-1][j]) + grid[i][j];
                 }
             }
             return grid[n-1][m-1];
