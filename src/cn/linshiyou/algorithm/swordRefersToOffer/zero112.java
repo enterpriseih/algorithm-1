e39716c8f818726d2ada0229727d56fb52a6e270
linshiyou <2335404839@qq.com>
2022-12-28 22:27:31 +0800

    剑指 Offer II 112. 最长递增路径 记忆化深度优先搜索

--- src/cn/linshiyou/algorithm/swordRefersToOffer/zero112.java
+++ src/cn/linshiyou/algorithm/swordRefersToOffer/zero112.java
@@ -7,16 +7,19 @@ import java.util.concurrent.*;
  * @Author: LJ
  * @Description: 剑指 Offer II 112. 最长递增路径
  *
- * bfs：单纯的暴力dfs不行，必须配合上动态规划，记录下每一个数字可以达到的极限。
+ * bfs：单纯的暴力dfs不行，必须配合上记忆
+ *
+ * 解法：记忆化深度优先搜索（在有向图中寻找最长路径问题）
  */
 public class zero112 {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
+
         int[][] matrix = {
-                {9,9,4},
-                {6,6,8},
-                {2,1,1},
+                {3,4,5},
+                {3,2,6},
+                {2,2,1},
         };
         int x = new Solution().longestIncreasingPath(matrix);
         System.out.println(x);
@@ -25,36 +28,50 @@ public class zero112 {
 }
 
 class Solution {
+
+    int[][] dp = null;
+
     public int longestIncreasingPath(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
+        dp =new int[m][n];
+        for (int i=0; i<m; i++)
+            Arrays.fill(dp[i], 1);
+
+
         int max = Integer.MIN_VALUE;
         for (int i=0; i<m; i++){
             for (int j=0; j<n; j++){
-                int temp = dfs(matrix, i, j, 1);
-                max = Math.max(max, temp);
+                max = Math.max(max,  dfs(matrix, i, j));
             }
         }
         return max;
     }
 
-    private int dfs(int[][] matrix, int row, int col, int step) {
+    private int dfs(int[][] matrix, int row, int col) {
+        if (dp[row][col]!=1) return dp[row][col];
         int cur = matrix[row][col];
-        int tempMax = step;
-        if (isCross(row-1, col, matrix) && cur<matrix[row-1][col]){
-            tempMax = dfs(matrix, row-1, col, step+1);
+
+        if (isCross(row-1, col, matrix) && cur < matrix[row-1][col]){
+            dp[row][col] = Math.max(dp[row][col], dfs(matrix, row-1, col)+1);
         }
-        if (isCross(row+1, col, matrix) && cur<matrix[row+1][col]){
-            tempMax = Math.max(tempMax, dfs(matrix, row+1, col, step+1));
+
+        if (isCross(row+1, col, matrix) && cur < matrix[row+1][col]){
+            dp[row][col] = Math.max(dp[row][col], dfs(matrix, row+1, col)+1);
         }
-        if (isCross(row, col-1, matrix) && cur<matrix[row][col-1]){
-            tempMax = Math.max(tempMax, dfs(matrix, row, col-1, step+1));
+
+        if (isCross(row, col-1, matrix) && cur < matrix[row][col-1]){
+            dp[row][col] = Math.max(dp[row][col], dfs(matrix, row, col-1)+1);
         }
-        if (isCross(row, col+1, matrix) && cur<matrix[row][col+1]){
-            tempMax = Math.max(tempMax, dfs(matrix, row, col+1, step+1));
+
+        if (isCross(row, col+1, matrix) && cur < matrix[row][col+1]){
+            dp[row][col] = Math.max(dp[row][col], dfs(matrix, row, col+1)+1);
         }
-        return tempMax;
+
+
+        return dp[row][col];
     }
+
 
     private boolean isCross(int row, int col, int[][] matrix){
         if (row>=0 && row<matrix.length && col>=0 && col<matrix[0].length) return true;
