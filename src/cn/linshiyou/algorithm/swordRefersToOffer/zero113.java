5af7ce29f1010811bd12876e78b27136d16a27ce
linshiyou <2335404839@qq.com>
2022-12-29 19:59:30 +0800

    剑指 Offer II 113. 课程顺序 拓扑排序

--- src/cn/linshiyou/algorithm/swordRefersToOffer/zero113.java
+++ src/cn/linshiyou/algorithm/swordRefersToOffer/zero113.java
@@ -43,7 +43,42 @@ public class zero113 {
             for (int[] info: prerequisites){
                 edges.get(info[1]).add(info[0]);
             }
+            // 每次挑选一个[未搜索]的节点，开始进行深度优先搜索
+            for (int i=0; i<numCourses && valid; i++){
+                if (visited[i]==0){
+                    dfs(i);
+                }
+            }
 
+            if (!valid){
+                return new int[0];
+            }
+            // 如果没有环，那么就有拓扑排序
+            return result;
+
+        }
+
+        private void dfs(int u) {
+            // 将结点标记为[搜索中]
+            visited[u] = 1;
+            // 搜索其相邻结点
+            // 只要发现有环，立刻停止搜索
+            for (int v : edges.get(u)){
+                // 如果 [未搜索]
+                if (visited[v]==0){
+                    dfs(v);
+                    if (!valid) return;
+                }
+                // 如果[搜索中]说明找到了环
+                else if (visited[v]==1){
+                    valid = false;
+                    return;
+                }
+            }
+            // 将结点标记为[已完成]
+            visited[u] = 2;
+            // 将结点入栈
+            result[index--] = u;
         }
     }
 }
