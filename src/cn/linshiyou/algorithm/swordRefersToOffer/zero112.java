ef5152214c2739d6d0baafb023e7593003022c84
linshiyou <2335404839@qq.com>
2022-12-31 00:51:13 +0800

    剑指 Offer II 114. 外星文字典 拓扑排序

--- src/cn/linshiyou/algorithm/swordRefersToOffer/zero112.java
+++ src/cn/linshiyou/algorithm/swordRefersToOffer/zero112.java
@@ -25,56 +25,56 @@ public class zero112 {
         System.out.println(x);
     }
 
-}
+    static class Solution {
 
-class Solution {
+        int[][] dp = null;
 
-    int[][] dp = null;
-
-    public int longestIncreasingPath(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        dp =new int[m][n];
-        for (int i=0; i<m; i++)
-            Arrays.fill(dp[i], 1);
+        public int longestIncreasingPath(int[][] matrix) {
+            int m = matrix.length;
+            int n = matrix[0].length;
+            dp =new int[m][n];
+            for (int i=0; i<m; i++)
+                Arrays.fill(dp[i], 1);
 
 
-        int max = Integer.MIN_VALUE;
-        for (int i=0; i<m; i++){
-            for (int j=0; j<n; j++){
-                max = Math.max(max,  dfs(matrix, i, j));
+            int max = Integer.MIN_VALUE;
+            for (int i=0; i<m; i++){
+                for (int j=0; j<n; j++){
+                    max = Math.max(max,  dfs(matrix, i, j));
+                }
             }
+            return max;
         }
-        return max;
+
+        private int dfs(int[][] matrix, int row, int col) {
+            if (dp[row][col]!=1) return dp[row][col];
+            int cur = matrix[row][col];
+
+            if (isCross(row-1, col, matrix) && cur < matrix[row-1][col]){
+                dp[row][col] = Math.max(dp[row][col], dfs(matrix, row-1, col)+1);
+            }
+
+            if (isCross(row+1, col, matrix) && cur < matrix[row+1][col]){
+                dp[row][col] = Math.max(dp[row][col], dfs(matrix, row+1, col)+1);
+            }
+
+            if (isCross(row, col-1, matrix) && cur < matrix[row][col-1]){
+                dp[row][col] = Math.max(dp[row][col], dfs(matrix, row, col-1)+1);
+            }
+
+            if (isCross(row, col+1, matrix) && cur < matrix[row][col+1]){
+                dp[row][col] = Math.max(dp[row][col], dfs(matrix, row, col+1)+1);
+            }
+
+
+            return dp[row][col];
+        }
+
+
+        private boolean isCross(int row, int col, int[][] matrix){
+            if (row>=0 && row<matrix.length && col>=0 && col<matrix[0].length) return true;
+            return false;
+        }
     }
 
-    private int dfs(int[][] matrix, int row, int col) {
-        if (dp[row][col]!=1) return dp[row][col];
-        int cur = matrix[row][col];
-
-        if (isCross(row-1, col, matrix) && cur < matrix[row-1][col]){
-            dp[row][col] = Math.max(dp[row][col], dfs(matrix, row-1, col)+1);
-        }
-
-        if (isCross(row+1, col, matrix) && cur < matrix[row+1][col]){
-            dp[row][col] = Math.max(dp[row][col], dfs(matrix, row+1, col)+1);
-        }
-
-        if (isCross(row, col-1, matrix) && cur < matrix[row][col-1]){
-            dp[row][col] = Math.max(dp[row][col], dfs(matrix, row, col-1)+1);
-        }
-
-        if (isCross(row, col+1, matrix) && cur < matrix[row][col+1]){
-            dp[row][col] = Math.max(dp[row][col], dfs(matrix, row, col+1)+1);
-        }
-
-
-        return dp[row][col];
-    }
-
-
-    private boolean isCross(int row, int col, int[][] matrix){
-        if (row>=0 && row<matrix.length && col>=0 && col<matrix[0].length) return true;
-        return false;
-    }
 }
