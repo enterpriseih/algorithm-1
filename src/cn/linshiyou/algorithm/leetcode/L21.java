550715d2d54a2d1bc642f58b757ac68bb34c7888
linshiyou <2335404839@qq.com>
2023-01-12 11:22:56 +0800

    21. 合并两个有序链表

--- src/cn/linshiyou/algorithm/leetcode/L21.java
+++ src/cn/linshiyou/algorithm/leetcode/L21.java
@@ -26,12 +26,11 @@ public class L21 {
                 if (list1.val> list2.val){
                     cur.next = new ListNode(list2.val);
                     list2 = list2.next;
-                    cur = cur.next;
                 }else {
                     cur.next = new ListNode(list1.val);
                     list1 = list1.next;
-                    cur = cur.next;
                 }
+                cur = cur.next;
             }
             if (list1==null)
                 cur.next = list2;
