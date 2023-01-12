ef4a91f30b5137e1bd92337e020be53549c11314
linshiyou <2335404839@qq.com>
2023-01-12 11:19:27 +0800

    21. 合并两个有序链表

---
+++ src/cn/linshiyou/algorithm/leetcode/L21.java
@@ -0,0 +1,56 @@
+package cn.linshiyou.algorithm.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @Author: LJ
+ * @Description: 21. 合并两个有序链表
+ *
+ * 构建一个新的链表，而后比较大小。
+ *
+ */
+public class L21 {
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+    static class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            ListNode ansList = new ListNode();
+            ListNode cur = ansList;
+
+            while (list1!=null && list2!=null){
+                if (list1.val> list2.val){
+                    cur.next = new ListNode(list2.val);
+                    list2 = list2.next;
+                    cur = cur.next;
+                }else {
+                    cur.next = new ListNode(list1.val);
+                    list1 = list1.next;
+                    cur = cur.next;
+                }
+            }
+            if (list1==null)
+                cur.next = list2;
+            else
+                cur.next = list1;
+
+            return ansList.next;
+        }
+    }
+
+
+    static class ListNode {
+         int val;
+         ListNode next;
+         ListNode() {}
+         ListNode(int val) { this.val = val; }
+         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     }
+
+
+
+}
