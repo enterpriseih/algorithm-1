29de9bb530b6a7a85335f3b5e917cce2cd654158
linshiyou <2335404839@qq.com>
2023-01-20 17:40:40 +0800

    567. 字符串的排列

--- src/cn/linshiyou/algorithm/leetcode/L567.java
+++ src/cn/linshiyou/algorithm/leetcode/L567.java
@@ -25,61 +25,47 @@ public class L567 {
     static class Solution {
         public boolean checkInclusion(String s1, String s2) {
             if (s1.length()>s2.length()) return false;
-            HashMap<Character, Integer> s1Map = new HashMap<>();
-            HashMap<Character, Integer> s2Map = new HashMap<>();
+            HashMap<Character, Integer> need = new HashMap<>();
+            HashMap<Character, Integer> window = new HashMap<>();
 
 
             for (Character c : s1.toCharArray()){
-                s1Map.put(c, s1Map.getOrDefault(c, 0)+1);
+                need.put(c, need.getOrDefault(c, 0)+1);
             }
 
             int left = 0;
-            int fast = s1.length();
+            int right = 0;
+            int valid = 0;
 
-            for (int i=left; i<fast; i++){
-                Character a = s2.charAt(i);
-                if (s1Map.containsKey(a)){
-                    s2Map.put(a, s2Map.getOrDefault(a, 0)+1);
-                    if (s1Map.get(a).equals(s2Map.get(a))){
-                        if (judge(s1Map, s2Map)) return true;
+            while (right<s2.length()) {
+
+                Character c = s2.charAt(right);
+                right++;
+                if (need.containsKey(c)) {
+                    window.put(c, window.getOrDefault(c, 0) + 1);
+                    if (need.get(c).equals(window.get(c))) {
+                        valid++;
                     }
                 }
-            }
 
+                // 左侧前进。
+                if (right - left == s1.length()) {
+                    if (valid == need.size()) return true;
 
-            while (fast<s2.length()){
+                    Character d = s2.charAt(left);
+                    left++;
 
-                Character old = s2.charAt(left);
-                if (s2Map.containsKey(old)){
-                    s2Map.put(old, s2Map.get(old)-1);
-                }
-                left++;
-
-                Character cur = s2.charAt(fast);
-                if (s1Map.containsKey(cur)){
-                    s2Map.put(cur, s2Map.getOrDefault(cur, 0)+1);
-                    if (s1Map.get(cur).equals(s2Map.get(cur))){
-                        if (judge(s1Map, s2Map)) return true;
+                    if (window.containsKey(d)) {
+                        if (need.get(d).equals(window.get(d))) {
+                            valid--;
+                        }
+                        window.put(d, window.get(d) - 1);
                     }
                 }
-                fast++;
-
             }
             return false;
         }
 
-        private boolean judge(HashMap<Character, Integer> s1Map, HashMap<Character, Integer> s2Map) {
-
-            Set<Character> set = s1Map.keySet();
-
-            for (Character a : set){
-                if (!s1Map.get(a).equals(s2Map.get(a))){
-                    return false;
-                }
-            }
-
-            return true;
-        }
     }
 
 }
